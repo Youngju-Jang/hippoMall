dae247fa1eedbdaabc0132ba4396f308de940d3e
Youngju-Jang <joj1043@nate.com>
2023-06-03 17:00:08 +0900

    [init] start project

---
+++ src/main/java/hello/spring/HippoMallApplication.java
@@ -0,0 +1,13 @@
+package hello.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HippoMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HippoMallApplication.class, args);
+	}
+
+}
